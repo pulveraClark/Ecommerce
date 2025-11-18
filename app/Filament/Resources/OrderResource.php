2cdd3d41b876c5c2060a2ba34b0966d8df3efc29
Clarkieboy <pulveraedzel@gmail.com>
2025-11-18 13:58:53 +0800

    Chapter 6

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Filament\Resources\OrderResource\RelationManagers\addressRelationManager;
 use App\Models\Order;
 use App\Models\Product;
 use Dom\Text;
@@ -280,7 +281,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            addressRelationManager::class
         ];
     }
 
