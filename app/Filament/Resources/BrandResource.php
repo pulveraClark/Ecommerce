98280d8aa44b4da2494e19721e0c395b4c241a97
Clarkieboy <pulveraedzel@gmail.com>
2025-11-18 09:12:26 +0800

    Chapter 3

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\Category; // if you reference Category
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\ActionGroup as TableActionGroup;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+
+
+class BrandResource extends Resource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-computer-desktop';
+
+    public static function form(Form $form): Form
+    {
+         return $form
+            ->schema([
+                Section::make([
+                    Grid::make()
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->maxLength(255)
+                                ->live(onBlur: true)
+                                ->afterStateUpdated(fn(string $operation, $state, $set) => 
+                                    $operation === 'create' ? $set('slug', \Illuminate\Support\Str::slug($state)) : null),
+
+                            Forms\Components\TextInput::make('slug')
+                                ->required()
+                                ->disabled()
+                                ->maxLength(255)
+                                ->dehydrated()
+                                ->unique(Brand::class, 'slug', ignoreRecord: true), 
+                        ]),
+
+                            FileUpload::make('image')
+                                ->image()
+                                ->directory('categories')
+                                ->maxSize(1024)
+                                ->nullable(),
+
+                            Toggle::make('is_active')
+                                ->label('Is Active')
+                                ->default(true),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+               
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                TableActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            'create' => Pages\CreateBrand::route('/create'),
+            'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Actions\ActionGroup;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Psy\Util\Str;
+use Spatie\FlareClient\View;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\ActionGroup as TableActionGroup;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make([
+                    Grid::make()
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->maxLength(255)
+                                ->live(onBlur: true)
+                                ->afterStateUpdated(fn(string $operation, $state, $set) => 
+                                    $operation === 'create' ? $set('slug', \Illuminate\Support\Str::slug($state)) : null),
+
+                            Forms\Components\TextInput::make('slug')
+                                ->required()
+                                ->disabled()
+                                ->maxLength(255)
+                                ->dehydrated()
+                                ->unique(Category::class, 'slug', ignoreRecord: true), 
+                        ]),
+
+                            FileUpload::make('image')
+                                ->image()
+                                ->directory('categories')
+                                ->maxSize(1024)
+                                ->nullable(),
+
+                            Toggle::make('is_active')
+                                ->label('Is Active')
+                                ->default(true),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                
+                Tables\Columns\ImageColumn::make('image'),
+
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+            
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                TableActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
