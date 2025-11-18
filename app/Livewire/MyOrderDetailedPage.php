b5b972096f63c31cf942e59e3a6e8c97e896f9f6
Clarkieboy <pulveraedzel@gmail.com>
2025-11-18 20:43:43 +0800

    Chapter 11

---
+++ app/Livewire/MyOrderDetailedPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MyOrderDetailedPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.my-order-detailed-page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ForgotPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
 use App\Livewire\Counter;
+use App\Livewire\MyOrderDetailedPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
+use App\Models\Product;
+use Filament\Support\Exceptions\Cancel;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +30,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);     
+Route::get('/checkout', CheckoutPage::class);  
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailedPage::class);
+
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
 Route::get('/counter', Counter::class);
     
 
