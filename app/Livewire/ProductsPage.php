639069379b1e46b8a004d9ee0cfcec4b1a6528ca
Clarkieboy <pulveraedzel@gmail.com>
2025-11-19 09:18:05 +0800

    Chapter 16

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -29,6 +29,9 @@ class ProductsPage extends Component
     #[Url]
     public $price_range = 300000;
 
+    #[Url]
+    public $sort = 'latest ';
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
@@ -51,6 +54,14 @@ class ProductsPage extends Component
         
         if( $this->price_range ) {
             $productQuery->whereBetween('price', [0, $this->price_range]);   
+        }
+
+        if($this->sort == 'latest'){
+            $productQuery->latest();
+        }
+
+        if($this->sort == 'price'){
+            $productQuery->orderBy('price');
         }
 
 
